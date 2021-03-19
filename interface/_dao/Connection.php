05a2a9f6f6250d6d5c880e69ea02ed6116839912
Romain Lefranc <rlefranc@lefresnoy.net>
2021-03-19 14:28:12 +0100

    add global variable for mysql base connection

--- interface/_dao/Connection.php
+++ interface/_dao/Connection.php
@@ -1,16 +1,20 @@
 <?php
 
 require_once(__DIR__.'/../exception/DAOException.php');
+require_once(__DIR__.'/../../config.php');
+
 
     class Connection{
         public static function connect() {
 
+            global $mysql_host, $mysql_database, $mysql_user;
+
             try {
-                $bdd = new PDO('mysql:
-                                host=localhost;
-                                dbname=panorama;
-                                charset=utf8', 
-                                'root', 
+                $bdd = new PDO("mysql:
+                                host=$mysql_host;
+                                dbname=$mysql_database;
+                                charset=utf8", 
+                                "$mysql_user", 
                                 '');
                 return $bdd;
             } catch(PDOException $e) {
