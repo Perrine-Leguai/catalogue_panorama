9b39e840584d9353088c3dd0f458998a4e15ce54
Perrine-Leguai <perrine.leguai@gmail.com>
2021-03-22 16:38:08 +0100

    sidebar corrected, working

--- interface/presentation/structure/sidebar.php
+++ interface/presentation/structure/sidebar.php
@@ -1,9 +1,9 @@
 <?php
     function sidebar($session=null, $student_list=null, $artwork_list=null){ 
         echo '<div class="col-2" style="height : 100%; min-height: 100vh;"> ';
-
+        
             //show student and artwork lists if user profil is staff
-            if(isset($session) && !empty($session) && $session['profil']=='is_staff'){ ?>
+            if(isset($session) && !empty($session) && $session=='is_staff'){ ?>
                 <div class="container-fluid column mt-5">
                 <!-- student List by alphabetic order -->
                     <label for="student_list"><h5>Liste des artistes</h5></label>
