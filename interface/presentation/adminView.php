0d41d39999d6ad237ec6a09a4aa76dd9a78d2a11
Perrine-Leguai <perrine.leguai@gmail.com>
2021-03-25 14:17:38 +0100

    improve the mark as seen button

--- interface/presentation/adminView.php
+++ interface/presentation/adminView.php
@@ -6,18 +6,25 @@
 
                     <div id="update<?=$i?>" class="update shadow p-3 mb-5 bg-white rounded">
                         <div class="row m-2">
-                            
+                            <label for="" class="col-3">Nom du champ modifié : </label>
+                            <p class="col-6 inputname h6 text-capitalize"><u><?=$update->getInputName()?></u></p>
                         </div>
                         <div class="row m-2">
-                            <p class="inputname h6 text-capitalize"><u><?=$update->getInputName()?></u></p>
-                            <div class="col-6 text-danger"><?=$update->getOldContent() ?></div>
-                            <div class="col-6 text-success"><?=$update->getNewContent() ?></div>
+                            
+                            <div class="col-6">
+                                <label for="old_content">Ancien contenu : </label>
+                                <div id="old_content" class="text-danger"><?=$update->getOldContent() ?></div>
+                            </div>
+                            <div class="col-6">
+                                <label for="new_content">Nouveau contenu :</label>
+                                <div id="new_content" class="text-success"><?=$update->getNewContent() ?></div>
+                            </div>
                         </div>
                         <div class="row mt-3 m-2">
                             <div class="d-inline-flex col-6"><?=$update->getUpdatedDate() ?></div>
                             <div class="d-inline-flex col-6 justify-content-end">
-                                <a type="sumbit" class="btn btn-light text-secondary" href="../_controller/adminViewController.php?type=update&edit=<?=$update->getId()?>">
-                                    <small>Marqué comme vu</small>
+                                <a id="turn_to_seen" type="sumbit" class="btn btn-outline-success text-secondary" href="../_controller/adminViewController.php?type=update&edit=<?=$update->getId()?>">
+                                    <small>Marquer comme vu</small>
                                 </a>
                             </div>
                         </div>
@@ -35,10 +42,11 @@
                             
                         </div>
                         <div class="row m-2">
+                            
                             <p class="inputname h6 text-capitalize"><u><?=$updated_artwork->getTitle()?></u></p>
-                            <div class="col-8 text-danger"><?=$updated_artwork->getSubtitle() ?></div>
-                            <div class="col-6 text-success"><?=$updated_artwork->getDuration() ?></div>
-                            <div class="col-6 text-success"><?=$updated_artwork->getType() ?></div>
+                            <div class="col-8 "><?=$updated_artwork->getSubtitle() ?></div>
+                            <div class="col-6 "><?=$updated_artwork->getDuration() ?></div>
+                            <div class="col-6 "><?=$updated_artwork->getType() ?></div>
                         </div>
                         <div class="row mt-3 m-2">
                             <div class="d-inline-flex "><?=$updated_artwork->getSynopsisShort() ?></div>
@@ -61,9 +69,3 @@
     
 <?php } ?>
 
-<?php function meanwhile($update){ ?>
-    <!-- <div class="d-inline-flex">
-    <?//$update->getFirstName()?>
-    </div> -->
-    <div class="d-inline-flex"> - <?=$update->getTitle()?></div>
-<?php } ?>
